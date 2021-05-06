5d830663f2a77a17a3b16d77f629cdf2093821e1
Ilya Taskaev <ansustiwaz@gmail.com>
2021-05-06 16:13:36 +0300

    on_failure

--- 3. Lesson Provisioners/Readme.md
+++ 3. Lesson Provisioners/Readme.md
@@ -26,3 +26,47 @@ Local Provisioners:
     command = "echo ${self.public_ip} >> private_ips.txt"
   }
 ```
+## Execute on Destroy
+
+If `when = destroy` is specified, the provisioner will run when the resource it is defined within is destroyed.
+
+
+```hcl
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo yum remove -y nano"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./EC2_Tutor-eu-north-1")
+    }
+  }
+```
+
+## Behavior on failure
+
+By default, provisioners that fail will also cause the Terraform apply itself to fail. The on_failure setting can be used to change this. The allowed values are:   
+
+`continue` - Ignore the error and continue with creation or destruction.   
+
+`fail` - Raise an error and stop applying (the default behavior). If this is a creation provisioner, taint the resource.   
+
+```hcl
+    provisioner "remote-exec" {
+    on_failure = continue
+    inline = [
+      "sudo amazon-linux-extras install -y nginx",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./EC2_Tutor-eu-north-1")
+    }
+  }
+```
--- 3. Lesson Provisioners/ec2.tf
+++ 3. Lesson Provisioners/ec2.tf
@@ -1,27 +1,86 @@
 resource "aws_instance" "myec2" {
-  ami           = data.aws_ami.amazon.id
-  instance_type = "t3.micro"
-  key_name      = var.key_name
+  ami                    = data.aws_ami.amazon.id
+  instance_type          = "t3.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   provisioner "remote-exec" {
     inline = [
       "sudo amazon-linux-extras install -y nginx1",
-      "sudo systemctl start nginx"
+      "sudo systemctl start nginx",
+      "sudo yum install -y nano"
     ]
 
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file("./EC2_Tutor-eu-north-1")
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./EC2_Tutor-eu-north-1")
+    }
   }
+
+    provisioner "remote-exec" {
+    on_failure = continue
+    inline = [
+      "sudo amazon-linux-extras install -y nginx",
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./EC2_Tutor-eu-north-1")
+    }
   }
+
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo yum remove -y nano"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("./EC2_Tutor-eu-north-1")
+    }
+  }
+
 
   provisioner "local-exec" {
     command = "echo ${self.private_ip} > private_ips.txt"
   }
-    provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo ${self.public_ip} >> private_ips.txt"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "allow_ssh security"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    self        = false
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    self        = false
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
   }
 }
 
